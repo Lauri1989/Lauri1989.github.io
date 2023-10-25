fd0c5476b774228ece19e69410ce6d137b4e7937
Lauri1989 <147425507+Lauri1989@users.noreply.github.com>
2023-10-25 21:04:33 +0300

    Lesson6
    Osa tunniteemast.

---
+++ Lesson6/Untitled-3.js
@@ -0,0 +1,15 @@
+//üks rida komentaar
+/*
+mitu rida
+kommentaari
+*/
+
+//muutujad ehk variables - elemendid mis hoiavad endas mingeid andmeid.
+/* ntx 5+6=11
+   x + 6 = 11
+   x + 6 = 11
+   x = 5
+   */
+  document.getElementById("varX").innerHTML = 5;
+  document.getElementById("varX2").innerHTML = 4;
+ 
